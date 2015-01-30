f50ced318a100c8cd5ace707322b3e0d0f0eeac8
Massimiliano Leone <maximilianus@gmail.com>
2015-01-30 04:59:52 +0100

    added a "PreExit" feature

--- src/net/iubris/heimdall/InteractiveConsole.java
+++ src/net/iubris/heimdall/InteractiveConsole.java
@@ -11,10 +11,12 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 import net.iubris.heimdall.actions.CommandAction;
 import net.iubris.heimdall.actions.HelpAction;
 import net.iubris.heimdall.actions.QuitAction;
+import net.iubris.heimdall.actions.QuitAction.PreExit;
 import net.iubris.heimdall.command.ConsoleCommand;
 import net.iubris.heimdall.listeners.CommandExceptionListener;
 
@@ -23,21 +25,46 @@ public class InteractiveConsole {
 	private static final String UNKNOWN_COMMAND = "Unknown command [%1$s]%n";
 	private static final String PROMPT = "facri> ";
 	
-	private final Map<Enum<? extends ConsoleCommand> ,CommandAction> actionsMap = new LinkedHashMap<>();
-	
+	private final Map<Enum<? extends ConsoleCommand>, CommandAction> actionsMap = new LinkedHashMap<>();
 	private final String title;
-
+	private QuitAction quitAction;
+	
+	public InteractiveConsole(String title, Set<CommandWithAction> commandsWithActions, Set<PreExit> preExits) {
+		this.title = "'"+title+"' available commands:";
+		for (CommandWithAction commandWithAction: commandsWithActions) {
+			actionsMap.put(commandWithAction.getConsoleCommand(), commandWithAction.getCommandAction());
+		}
+		quitAction = new QuitAction(preExits);
+		actionsMap.put(QuitAction.QuitCommand.Q, quitAction);
+	}
+	
+	public InteractiveConsole(String title, CommandWithAction commandWithAction, PreExit preExit) {
+		this.title = "'"+title+"' available commands:";
+//		for (CommandWithAction commandWithAction: commandsWithActions) {
+			actionsMap.put(commandWithAction.getConsoleCommand(), commandWithAction.getCommandAction());
+//		}
+		quitAction = new QuitAction(preExit);
+		actionsMap.put(QuitAction.QuitCommand.Q, quitAction);
+	}
+	
 	public InteractiveConsole(String title, CommandWithAction... commandsWithActions) {
 		this.title = "'"+title+"' available commands:";
 		for (CommandWithAction commandWithAction: commandsWithActions) {
 			actionsMap.put(commandWithAction.getConsoleCommand(), commandWithAction.getCommandAction());
 		}
+		quitAction = new QuitAction();
+		actionsMap.put(QuitAction.QuitCommand.Q, quitAction);
 	}
+	
+	public void addPreExit(PreExit preExit) {
+		quitAction.addPreExit(preExit);
+	}
+	
+	
 	
 	public void execCommandLoop(final Console console) {
 		HelpAction helpAction = new HelpAction(title, actionsMap.keySet() );
 		actionsMap.put(HelpAction.HelpCommand.H, helpAction);
-		actionsMap.put(QuitAction.QuitCommand.Q, new QuitAction());
 		helpAction.exec(console);
 //		System.out.println("");
 //		console.printf("\n");
@@ -86,4 +113,9 @@ public class InteractiveConsole {
 			scanner.close();
 		}// end while
 	}// end method
+	
+//	@FunctionalInterface
+//	public interface PreExit {
+//		void doPreExiting();
+//	}
 }
--- src/net/iubris/heimdall/actions/QuitAction.java
+++ src/net/iubris/heimdall/actions/QuitAction.java
@@ -1,15 +1,72 @@
 package net.iubris.heimdall.actions;
 
 import java.io.Console;
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 import net.iubris.heimdall.command.ConsoleCommand;
 
 public class QuitAction implements CommandAction {
 	
+	private final Set<PreExit> preExits;
+	
+	private final Set<String> errorsStrings = new HashSet<>();
+	private final File errorsFile = new File("errors.txt");
+	
+	public QuitAction(Set<PreExit> preExits) {
+		this.preExits = preExits;
+	}
+	
+	public QuitAction(PreExit preExit) {
+		this.preExits = new HashSet<>();
+		preExits.add(preExit);
+	}
+	
+	public QuitAction() {
+		preExits = new HashSet<>();
+//		PrintStream errorPrintStream = null; 
+//		try {
+//			errorPrintStream = new PrintStream(errorsFile) {
+//				@Override
+//				public void println(String x) {
+//					if (!errorsStrings.contains(x)) {
+//						errorsStrings.add(x);
+//						if (!errorsFile.exists()) {
+//							try {
+//								errorsFile.createNewFile();
+//							} catch (IOException e) {
+//								System.out.println("unable to create "+errorsFile.getName()+"; check your filesystem");
+//							}
+//						}
+//						super.println(x);
+//					}				
+//				}			
+//			};
+//		} catch (IOException e) {
+//			System.out.println(e.getMessage());
+//		}
+//		System.setErr(errorPrintStream);
+	}
+	
+	public void addPreExit(PreExit preExit) {
+		preExits.add(preExit);
+	}
+	
 	@Override
 	public void exec(Console console, String... params) {
+		if (errorsStrings.size()>0) {
+			console.printf("some errors occured; see '"+errorsFile.getName()+"'");
+		}
+		if (preExits.size()>0)
+			preExits.stream().forEach(p->p.doPreExiting());
 		console.printf("Bye%n");
 		System.exit(0);
+	}
+	
+	@FunctionalInterface
+	public interface PreExit {
+		void doPreExiting();
 	}
 	
 	public enum QuitCommand implements ConsoleCommand {
